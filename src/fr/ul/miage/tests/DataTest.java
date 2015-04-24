ce7145a7f8e54e8359c0a041b00d27eb60381237
Maxime BLAISE <maxime.blaise12@gmail.com>
2015-04-24 14:35:44 +0200

    Ecriture d'une classe de tests

---
+++ src/fr/ul/miage/tests/DataTest.java
@@ -0,0 +1,27 @@
+
+package fr.ul.miage.tests;
+
+// Tous les imports nécessaire
+import fr.ul.miage.projet.*;
+
+/**
+ * Classe de test pour vérifier si les "datas" sont bien interprétés.
+ */
+public class DataTest {
+
+	/**
+	 * Méthode principale, créer une TDS, un arbre vide et génère l'assembleur!
+	 */
+	public static void main(String[] args) {
+		// Création d'une table des symboles avec des variables globales
+		TableDesSymboles tds = new TableDesSymboles();
+		tds.insertion("i", 0, "var", "5");
+		// Création d'un arbre vide
+		Arbre a = new Arbre();
+		// Traduction vers le code assembleur !
+		Assembleur asm = new Assembleur(a, tds);
+		asm.generer_prog();
+		// Affichage du résultat
+		System.out.println(asm);
+	}
+}
