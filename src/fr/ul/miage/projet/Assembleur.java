5fdc25543d97b787d05eaefe9f5daa31db37e3c3
alphonse <pierresara7@gmail.com>
2015-04-24 15:24:52 +0200

    modif classe assembleur

--- src/fr/ul/miage/projet/Assembleur.java
+++ src/fr/ul/miage/projet/Assembleur.java
@@ -53,7 +53,7 @@ public class Assembleur {
  	
  	 // test si un caractere est une constante
 
- 	public boolean estChiffre(String string) {
+ 	public boolean static estChiffre(String string) {
 		try {
 			Integer.parseInt(string + "");
 			return true;
